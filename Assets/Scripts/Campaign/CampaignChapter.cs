bc9fab595d20e2bdf0db53a152c2ff5f4a5b51d7
Landelo <ccastles.ve@mksb.tech>
2023-06-06 11:59:37 +0300

    campaign chapter

--- Assets/Scripts/Campaign/CampaignChapter.cs
+++ Assets/Scripts/Campaign/CampaignChapter.cs
@@ -1,12 +1,11 @@
-﻿using System.Collections;
+﻿using Sirenix.Serialization;
 using System.Collections.Generic;
 using UnityEngine;
-using Sirenix.OdinInspector;
-using Sirenix.Serialization;
 [CreateAssetMenu(fileName = "NewChapter", menuName = "Chapter/Campaign Chapter", order = 55)]
-public class CampaignChapter : ScriptableObject{
-	public string Name;
-	public int numChapter; 
-	[OdinSerialize] public List<CampaignMission> missions = new List<CampaignMission>();
+public class CampaignChapter : ScriptableObject
+{
+    public string Name;
+    public int numChapter;
+    [OdinSerialize] public List<CampaignMission> missions = new List<CampaignMission>();
 
 }
