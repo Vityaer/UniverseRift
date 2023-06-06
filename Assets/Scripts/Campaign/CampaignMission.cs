77320681c070f5f0d40167f7e3f6ba9697f4b7c2
Landelo <ccastles.ve@mksb.tech>
2023-06-06 12:10:38 +0300

    campaign mission

--- Assets/Scripts/Campaign/CampaignMission.cs
+++ Assets/Scripts/Campaign/CampaignMission.cs
@@ -1,21 +1,22 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
-using Sirenix.OdinInspector;
-using Sirenix.Serialization;
+
 [System.Serializable]
-public class CampaignMission : Mission, ICloneable{
+public class CampaignMission : Mission, ICloneable
+{
+    [Header("Auto fight reward")]
+    [SerializeField] private AutoReward _autoFightReward;
 
+    public AutoReward AutoFightReward => _autoFightReward;
 
-	[Header("Auto fight reward")]
-	[SerializeField] private AutoReward autoFightReward;
-	public AutoReward AutoFightReward{get => autoFightReward;}
-	public object Clone(){
-        return new CampaignMission  { 	Name = this.Name,
-        							 	_listEnemy = this.listEnemy,
-        							 	winReward     = (Reward) this.WinReward.Clone(),
-        							 	autoFightReward  = this.autoFightReward,
-        								location = this.location
-        							};				
+    public object Clone()
+    {
+        return new CampaignMission
+        {
+            Name = this.Name,
+            _listEnemy = this.listEnemy,
+            winReward = (Reward)this.WinReward.Clone(),
+            _autoFightReward = this._autoFightReward,
+            location = this.location
+        };
     }
 }
